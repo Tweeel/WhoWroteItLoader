ef55f73d40eec9566f46e167f96e6478c8315066
Baba Bendermel Houssem Eddine <75279465+Tweeel@users.noreply.github.com>
2021-10-08 20:47:22 +0100

    .

---
+++ app/src/main/java/com/example/whowroteitloader/BookLoader.java
@@ -0,0 +1,33 @@
+package com.example.whowroteitloader;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.loader.content.AsyncTaskLoader;
+
+public class BookLoader extends AsyncTaskLoader<String> {
+
+    private String mQueryString;
+
+    BookLoader(@NonNull Context context, String queryString) {
+        super(context);
+        mQueryString = queryString;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+
+        forceLoad();
+    }
+
+    @Nullable
+    @Override
+    public String loadInBackground() {
+        return NetworkUtils.getBookInfo(mQueryString);
+    }
+
+
+
+}
